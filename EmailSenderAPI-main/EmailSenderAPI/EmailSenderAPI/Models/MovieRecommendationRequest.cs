1f65ee11ec7b66367c6ca2c655919b64c24a2e1c
TanyaRo12 <taro@sfedu.ru>
2025-12-11 15:40:19 +0300

    Add files via upload

---
+++ EmailSenderAPI-main/EmailSenderAPI/EmailSenderAPI/Models/MovieRecommendationRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace EmailSenderAPI.Models
+{
+    public class MovieEmailRequest
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Date { get; set; } = string.Empty;
+        public string MovieTitle { get; set; } = string.Empty;
+        public string MovieDescription { get; set; } = string.Empty;
+        public string MovieGenre { get; set; } = string.Empty;
+        public string ViewingTime { get; set; } = "20:00";
+    }
+}
---
+++ EmailSenderAPI-main/EmailSenderAPI/EmailSenderAPI/Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.FileProviders;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// ДОБАВЬ ЭТО
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+var app = builder.Build();
+
+// ИСПОЛЬЗУЙ CORS
+app.UseCors("AllowAll");
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.UseStaticFiles();
+app.UseDefaultFiles();
+
+app.MapControllers();
+app.MapFallbackToFile("index.html");
+
+app.Run("http://localhost:7200");
